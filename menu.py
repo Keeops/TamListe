f4c2db643b4d2f2df8808535cb1f37a5a07b1bb2
Keeops <emretok35@gmail.com>
2021-04-09 02:17:46 +0300

    NEEDBUGFIXES

---
+++ menu.py
@@ -0,0 +1,107 @@
+import pygame
+import test
+import os
+pygame.font.init()
+pygame.mixer.init()
+pygame.display.init()
+
+# RESULATION,WINDOW SETTINGS
+FPS = 60
+WIDTH, HEIGHT = 1920, 1080
+IMG_WIDTH, IMG_HEIGHT = 101, 151
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN | pygame.SCALED)
+pygame.display.set_caption("Save the Asude")
+
+# INITIALIZING IMAGES
+BATU_RAW = pygame.image.load(os.path.join("assets", "batur.png"))
+UMUT_RAW = pygame.image.load(os.path.join("assets", "umut.jpg"))
+BACKGROUND_RAW = pygame.image.load(os.path.join("assets", "background.jpg"))
+BATU = pygame.transform.scale(BATU_RAW, (IMG_WIDTH,IMG_HEIGHT))
+UMUT = pygame.transform.scale(UMUT_RAW, (IMG_WIDTH,IMG_HEIGHT))
+BACKGROUND = pygame.transform.scale(BACKGROUND_RAW, (WIDTH,HEIGHT))
+PLATFORM_RAW = pygame.image.load(os.path.join("assets", "platform.png"))
+
+# INITIALIZING FONTS
+## HIT = pygame.USEREVENT + 1
+
+
+
+# COLORS
+WHITE = (255, 255, 255)
+RED = (255,0,0)
+BLACK = (0,0,0)
+
+def draw_menu(buttons):
+	screen.blit(BACKGROUND, (0,0))
+	for button in buttons:
+		button.draw()
+	pygame.display.update()
+
+class Button(object):
+	"""docstring for PlayButton"""
+	def __init__(self, x, y, size, color, text):
+		super().__init__()
+		self.x = x
+		self.y = y
+		self.size = size
+		self.color = color
+		self.text = text
+		self.Font = pygame.font.SysFont("comicsans", self.size)
+		self.Text = self.Font.render(text,1,self.color)
+		self.fontwidth = pygame.font.Font.size(self.Font, text)
+		self.rect = pygame.Rect(self.x, self.y, self.fontwidth[0], self.fontwidth[1])
+
+	def draw(self):
+		screen.blit(self.Text, (self.x, self.y))
+
+
+def main():
+	run = True
+	clicking = False
+	clock = pygame.time.Clock()
+	playbutton = Button(150, 400, 100, BLACK, "Play")
+	settingsbutton = Button(150, 500, 100,BLACK,"Settings")
+	quitbutton = Button(150, 600, 100,BLACK,"Quit")
+	buttons = [playbutton,settingsbutton,quitbutton]
+	while (run):
+		clock.tick(FPS)
+		draw_menu(buttons)
+		for event in pygame.event.get():
+			if (event.type == pygame.QUIT):
+				run = False
+				pygame.quit()
+			
+			if (event.type == pygame.KEYDOWN):
+				if (event.key == pygame.K_ESCAPE):
+					run = False
+					pygame.quit()
+			if (event.type == pygame.MOUSEBUTTONDOWN):
+				# Left click = 1
+				# Middle click = 2
+				# Right click = 3
+				# Scroll up = 4
+				# Scroll down = 5
+				if (event.button == 1):
+					clicking = True
+			
+			if (event.type == pygame.MOUSEBUTTONUP):
+				if (event.button == 1):
+					mloc = pygame.mouse.get_pos()
+					clicked_rects = [b for b in buttons if b.rect.collidepoint(mloc)]
+					
+					# Play button 
+					if (clicked_rects and clicked_rects[0] == playbutton):
+						test.main()
+
+					# Settings button
+					elif (clicked_rects and clicked_rects[0] == settingsbutton):
+						pass
+
+					# Quit Button
+					elif (clicked_rects and clicked_rects[0] == quitbutton):
+						pygame.quit()
+		
+					clicking = False
+
+if __name__ == '__main__':
+	main()
--- test.py
+++ test.py
@@ -1,13 +1,25 @@
 import pygame
+import menu
 import os
 pygame.font.init()
 pygame.mixer.init()
 pygame.display.init()
 
+"""
+Bugs:
+
+	Player going through inside the platforms
+	Player can jump under the platforms
+
+
+"""
+
+
+
 # RESULATION,WINDOW SETTINGS
 FPS = 60
 WIDTH, HEIGHT = 1920, 1080
-IMG_WIDTH, IMG_HEIGHT = 101, 151
+IMG_WIDTH, IMG_HEIGHT = 100, 150
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN | pygame.SCALED)
 pygame.display.set_caption("Save the Asude")
 
@@ -52,12 +64,10 @@ class Player(object):
 		self.collide_list = []
 
 	def update(self, platform_group):
-		self.rect.x = self.x
-		self.rect.y = self.y
 
 		if (self.jumping):
 			if (self.jumpValue > 0):
-				self.y -= self.jumpValue
+				self.rect.y -= self.jumpValue
 				self.jumpValue -= 1
 			else:
 				self.jumpValue = 20
@@ -66,38 +76,81 @@ class Player(object):
 
 		elif (self.falling and not self.collide_list):
 			if (self.fallValue <= 5):
-				self.y += self.fallValue
+				self.rect.y += self.fallValue
 				self.fallValue += 1
 			if (self.fallValue > 5):
-				self.y += 5
+				self.rect.y += 5
 				self.jumping = False
 				self.falling = True
 
 		# if the player on air
-		elif (self.y + 5 + IMG_HEIGHT -5 <= 1080 and not self.collide_list):
-			self.y += 5
+		elif (self.rect.y + 5 + IMG_HEIGHT <= 1080-30 and not self.collide_list):
+			self.rect.y += 5
 			self.falling = True
 			self.jumping = False
 
-		# if the player on ground or platform
+		# if the player on ground
 		elif (self.collide_list):
-			self.falling = False
-			self.jumping = False
 			if (self.rect.bottom == self.collide_list[0].rect.top):
 				self.rect.bottom = self.collide_list[0].rect.top
-				
+
+			self.falling = False
+			self.jumping = False
+
+		
+
 
 	def draw(self):
-		screen.blit(self.img, (self.x, self.y))
+		screen.blit(self.img, (self.rect.x, self.rect.y))
 
-	def move_left(self):
-		if (self.x - self.Velocity >= 0):
-			self.x -= self.Velocity
+	def move_left(self,platform_group,ground):
+		# if the player is on the platform
+		if (self.collide_list and (ground not in self.collide_list) and self.rect.bottom == self.collide_list[0].rect.top + 5):
+			self.rect.x -= self.Velocity
 
-	def move_right(self):				#   1770
-		if (self.x + self.Velocity <= 1920-IMG_WIDTH+1):
-			self.x += self.Velocity
-	
+		# if the player is on the air
+		elif (not self.collide_list):
+			self.rect.x -= self.Velocity
+
+		# if the player on air and platform is left of the player
+		elif (ground not in self.collide_list and self.collide_list):
+			if (self.rect.x == self.collide_list[0].rect.right):
+				self.rect.x = self.collide_list[0].rect.right
+				self.falling = True
+
+		# if the player on ground and platform is left of the player
+		elif (ground in self.collide_list and len(self.collide_list) >= 2):
+			if (self.rect.x == self.collide_list[1].rect.right):
+				self.rect.x = self.collide_list[1].rect.right
+		
+		# if the player on the ground
+		elif (self.rect.x - self.Velocity <= 1920-IMG_WIDTH+1) and (ground in self.collide_list):
+			self.rect.x -= self.Velocity
+
+	def move_right(self,platform_group, ground):
+		# if the player is on the platform
+		if (self.collide_list and (ground not in self.collide_list) and self.rect.bottom == self.collide_list[0].rect.top + 5):
+			self.rect.x += self.Velocity
+
+		# if the player is on the air
+		elif (not self.collide_list):
+			self.rect.x += self.Velocity
+
+		# if the player on air and platform is right of the player
+		elif (ground not in self.collide_list and self.collide_list):
+			if (self.rect.x + IMG_WIDTH == self.collide_list[0].rect.left):
+				self.rect.x = self.collide_list[0].rect.left - IMG_WIDTH
+				self.falling = True
+
+		# if the player on ground and platform is right of the player
+		elif (ground in self.collide_list and len(self.collide_list) >= 2):
+			if (self.rect.x + IMG_WIDTH == self.collide_list[1].rect.left):
+				self.rect.x = self.collide_list[1].rect.left - IMG_WIDTH
+		
+		# if the player on the ground
+		elif (self.rect.x + self.Velocity <= 1920-IMG_WIDTH+1) and (ground in self.collide_list):
+			self.rect.x += self.Velocity
+
 	def jump(self):
 		self.jumping = True
 		self.falling = False
@@ -117,7 +170,12 @@ class Platform(object):
 		self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
 	def draw(self):
-		screen.blit(self.img, (self.x, self.y))
+		screen.blit(self.img, (self.rect.x, self.rect.y))
+
+
+
+
+
 
 def draw_window(batu,platform_group):
 	screen.blit(BACKGROUND, (0,0))
@@ -136,17 +194,24 @@ def main():
 	batu = Player(IMG_WIDTH, IMG_HEIGHT, 0, 1080-IMG_HEIGHT-530)
 	ground = Platform(1920, 30, 0, 1080-30)
 	platform1 = Platform(200, 30, 300, 950)
-	platform_group = [ground,platform1]
+	platform2 = Platform(200, 30, 700, 750)
+	platform3 = Platform(200, 30, 1100, 550)
+	platform4 = Platform(200, 30, 1500, 550)
+	platform5 = Platform(200, 30, 1700, 350)
+	platform6 = Platform(200, 30, 1200, 150)
+	platform7 = Platform(200, 30, 600, 150)
+	platform8 = Platform(200, 30, 200, 150)
+	platform_group = [ground,platform1,platform2,platform3,platform4,platform5,platform6,platform7,platform8]
 	while (run):
 		clock.tick(FPS)
 		draw_window(batu,platform_group)
-		
+		print(batu.rect.right, platform_group[1].rect.left)
 		keys_pressed = pygame.key.get_pressed()
 		# Horizantally movement
 		if (keys_pressed[pygame.K_a]):
-			batu.move_left()
+			batu.move_left(platform_group,ground)
 		elif (keys_pressed[pygame.K_d]):
-			batu.move_right()
+			batu.move_right(platform_group,ground)
 
 		# Check collide to platform
 		for platform in platform_group:
@@ -163,15 +228,12 @@ def main():
 			if (event.type == pygame.KEYDOWN):
 				if (event.key == pygame.K_ESCAPE):
 					run = False
-					pygame.quit()
+					menu.main()
 				
 				if (event.key == pygame.K_w or event.key == pygame.K_SPACE):
-					print(batu.jumping, batu.falling)
 					if not(batu.jumping or batu.falling) and batu.collide_list:
 						batu.jump()
 						
-				if (event.key == pygame.K_d):
-					batu.move_right()
 
 if __name__ == '__main__':
 	main()
